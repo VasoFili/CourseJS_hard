44a3d844b72ba9be9a9a8c7c562d3e258f47ff71
Vasiliy Filippov <Filippoff.v@mail.ru>
2020-08-10 14:34:40 +0300

    fix(lesson02hard): created the correct commit

--- script/script.js
+++ script/script.js
@@ -1,19 +1,19 @@
-let num = 266219;
-let string = String(num);
-let multi = 1;
-let power = 3;
-let resultPower;
-let stringResultPower;
+const num = 266219;
+const string = String(num);
+const array = string.split("");
 
-for (let i = 0; i < string.length; i++) {
-   multi = multi * Number(string[i]);
-};
+// let result = array.reduce(function (multi, current) {
+//    return multi * current;
+// }, 1);
+// console.log(result);
 
-console.log(multi);
+const reducer = (multi, current) => multi * current;
+console.log(array.reduce(reducer));
 
-resultPower = multi ** power;
-console.log(resultPower);
 
-stringResultPower = String(resultPower);
 
-console.log(stringResultPower.substr(0, 2));
+// const power = array.reduce(reducer) ** 3;
+
+// alert(String(array.reduce(reducer) ** 3).substring(0, 2));
+
+alert((String(array.reduce(reducer) ** 3).split("")).slice(0, 2));
